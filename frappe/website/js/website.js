752259797b8c39c49643aff9fe81afd83055dbd8
Ronel Cabrera <ronelvcabrera007@gmail.com>
2019-03-13 09:54:09 +0800

    escape error

--- frappe/website/js/website.js
+++ frappe/website/js/website.js
@@ -429,7 +429,7 @@ $(document).ready(function() {
 
 $(document).on("page-change", function() {
 	$(document).trigger("apply_permissions");
-	$('.dropdown-toggle').dropdown();
+	// $('.dropdown-toggle').dropdown();
 
 	//multilevel dropdown fix
 	$('.dropdown-menu .dropdown-submenu .dropdown-toggle').on('click', function(e) {
